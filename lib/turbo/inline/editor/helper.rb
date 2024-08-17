b876dd7a2365e7d68357f588c365e9bec52a2681
Igor Zubkov <igor.zubkov@gmail.com>
2024-08-17 10:22:23 +0200

    Stub turbo_inline_editor and turbo_inline_editor_if

---
+++ lib/turbo/inline/editor/helper.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Turbo
+  module Inline
+    module Editor
+      # @params model [Object]
+      # @param field [Object]
+      # @param options [Hash] Default: {}
+      def turbo_inline_editor(model, field, options = {})
+      end
+
+      # @param condition [Object]
+      # @params model [Object]
+      # @param field [Object]
+      # @param options [Hash] Default: {}
+      def turbo_inline_editor_if(condition, object, field, options = {})
+      end
+    end
+  end
+end
--- spec/turbo/inline/editor_spec.rb
+++ spec/turbo/inline/editor_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Turbo::Inline::Editor do
     expect(Turbo::Inline::Editor::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
 end
